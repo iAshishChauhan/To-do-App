8a619118cd46a76c009f7e92e2f1457729d7e2df
iAshishChauhan <iashishsinghchauhan@gmail.com>
2019-11-23 22:32:58 +0530

    Multiple task deletion functionlity

--- assets/js/home.js
+++ assets/js/home.js
@@ -1,6 +1,21 @@
 // console.log('It works');
 
 function sendId() {
-    let id = document.getElementById('task-id').value;
-    window.location.href = "/delete-task/?id="+id;
+    let checkboxes = document.getElementsByName('selectedbox');
+    // console.log(id);
+    let temp="";
+    let first = true;
+    for (var i=0; i<checkboxes.length; i++) {
+        // And stick the checked ones onto an array...
+        if (checkboxes[i].checked) {
+            if(first){
+                temp = "id="+checkboxes[i].value;
+                first = false;
+            }else{
+                temp += "&id="+checkboxes[i].value;
+            }
+        }
+    }
+    console.log(temp);
+    window.location.href = "/delete-task/?"+temp;
 }
--- controllers/deleteTask_controller.js
+++ controllers/deleteTask_controller.js
@@ -2,11 +2,14 @@ const Task = require('../models/Task');
 
 module.exports.deleteTask = function(req,res) {
     let id = req.query.id;
-    Task.findByIdAndDelete(id, function(err){
-        if(err) {
-            console.log('Error in deleting the object from Database');
-            return;
-        }
-        return res.redirect('back');
-    });
+    console.log(id);
+    for(let i=0;i<id.length;i++){
+        Task.findByIdAndDelete(id[i], function(err){
+            if(err) {
+                console.log('Error in deleting the object from Database');
+                return;
+            }
+        });
+    }
+    return res.redirect('back');
 }
