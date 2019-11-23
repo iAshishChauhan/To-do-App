5bfe9dcd3b58347fb069c84795bc409068390618
iAshishChauhan <iashishsinghchauhan@gmail.com>
2019-11-23 20:05:16 +0530

    revert commit

---
+++ tasks.js
@@ -0,0 +1,17 @@
+module.exports.Tasks = [
+    {
+        description: "Do Homework",
+        category: "Work",
+        duedate: "02 may 2019"
+    },
+    {
+        description: "Buy Milk",
+        category: "personal",
+        duedate: "02 june 2019"
+    },
+    {
+        description: "Go shopping",
+        category: "personal",
+        duedate: "02 july 2019"
+    }
+];
