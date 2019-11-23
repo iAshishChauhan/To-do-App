72edef716ea9e696ae000e4e2ebc5db2fa7c0d75
iAshishChauhan <iashishsinghchauhan@gmail.com>
2019-11-23 19:57:25 +0530

    rev

--- routes/index.js
+++ routes/index.js
@@ -2,10 +2,8 @@ const express = require('express');
 const router = express.Router();
 const homeController = require('../controllers/home_controller');
 const addTaskController = require('../controllers/addTask_controller');
-const deleteTaskController = require('../controllers/deleteTask_controller');
 
 router.get('/', homeController.home);
 router.post('/add-task', addTaskController.AddTask);
-router.get('/delete-task', deleteTaskController.deleteTask);
 
 module.exports = router;
